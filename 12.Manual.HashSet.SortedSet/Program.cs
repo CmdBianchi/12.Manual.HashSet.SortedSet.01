2d579a7bc395197d4a6d3c30ff9ae7eec7c95e43
marcu <marcu@LAPTOP-MTO8J5A0>
2021-07-21 09:18:56 -0300

    Adicionar arquivos de projeto.

---
+++ 12.Manual.HashSet.SortedSet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _12.Manual.HashSet.SortedSet {
+    class Program {
+        static void Main(string[] args) {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook")); // Verifica se esta dentro do conjunto.
+            foreach (string p in set) {
+                Console.WriteLine(p);
+            }
+           
+        }
+    }
+}
